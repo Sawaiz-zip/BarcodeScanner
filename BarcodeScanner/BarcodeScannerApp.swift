b99573a45dcb104a1ef2448e33154cfdf539d0e0
Sawaiz Naveed <sawaiznaveed1@gmail.com>
2024-01-04 03:04:11 +0100

    Initial Commit

---
+++ BarcodeScanner/BarcodeScannerApp.swift
@@ -0,0 +1,17 @@
+//
+//  BarcodeScannerApp.swift
+//  BarcodeScanner
+//
+//  Created by Sawaiz Naveed on 1/4/24.
+//
+
+import SwiftUI
+
+@main
+struct BarcodeScannerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
